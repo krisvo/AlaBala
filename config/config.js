f012abbbb39b33e7e911aeb71e11cd8a840a3668
lmarinov <franki123>
2017-04-25 17:03:28 +0300

    Moved user details file.

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports={
     development:{
         rootFolder: path.normalize(path.join(__dirname,'/../')),
-        connectionString: 'mongodb://localhost:27017/blog'
+        connectionString: 'mongodb://localhost:27017/Blog'
     },
     production:{}
 };
