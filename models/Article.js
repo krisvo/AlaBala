1b77a56a1e80c53ef2f064d28ad80f49928f9608
joroka <joroka_@abv.bg>
2017-04-26 18:53:28 +0300

    upload images/cat1.jpg

--- models/Article.js
+++ models/Article.js
@@ -7,7 +7,8 @@ let articleSchema = mongoose.Schema({
     content: {type:String, required: true},
     author: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
     comments: [{type:mongoose.Schema.Types.ObjectId, ref:'Comment'}],
-    date: {type:Date, default: Date.now()}
+    date: {type:Date, default: Date.now()},
+    imagePath: {type: String},
 });
 
 const Article = mongoose.model ('Article',articleSchema);
